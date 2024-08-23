b6abf8e3ad95f5c7e4b86820a0d2010ebc5b1531
Aristote Ngoy <harriscreativity@gmail.com>
2024-08-24 01:58:54 +0200

    new animations

--- src/app/(dashboard)/numbers/index.tsx
+++ src/app/(dashboard)/numbers/index.tsx
@@ -1,23 +1,42 @@
 import { View } from "@/components/View";
-import { Text } from "@/components/Text";
-import { StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
+import AnimatedCurrency from "./components/animated.currency";
+import DigitalInputs from "./components/digital.inputs";
+import { useCallback, useState } from "react";
 
 export default function NumbersScreen() {
+
+    const [amount, setAmount] = useState(0)
+
+    const onValueChange = useCallback(
+      (value: string) => {
+        console.log(value)
+        setAmount(Number(value))
+      },[])
+    
+
     return(
-        <View style={styles.container}>
-            <Text>Numbers</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={styles.container}>
+                <AnimatedCurrency amount={amount} index={0} />
+                <DigitalInputs onValueChange={onValueChange} />
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: 100,
         alignItems: 'center',
         justifyContent: 'center',
+        gap: 30
     },
     title: {
         fontSize: 20,
         fontWeight: 'bold',
     },
 })
+
+
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -20,5 +20,30 @@ export const Items : Item[] = [
     {
         title: 'Animated Cards',
         route: '(dashboard)/cards'
+    },
+    {
+        title: 'Animated Expanded',
+        route: '(dashboard)/expanded'
+    },
+    {
+        title: 'Animated List View',
+        route: '(dashboard)/listview'
     }
 ]
+
+
+//south africa zar
+export const formatter = new Intl.NumberFormat('en-ZA');
+
+export const getKey = (formattedIndex: number, original: number) => {
+  const formatted = formatter.format(original);
+  // if a dot, do nothing
+  if (formatted[formattedIndex] === '.') return `.-${formattedIndex}`;
+  // find the index of the digit in the original number
+  let index = 0;
+  for (let i = 0; i < formattedIndex; i++) {
+    if (formatted[i] === '.') continue;
+    index++;
+  }
+  return index;
+};
